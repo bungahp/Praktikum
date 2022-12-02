570bf015e282aad306b5a156e6e8eee7543d7193
bungahp <111692286+bungahp@users.noreply.github.com>
2022-12-03 00:11:08 +0700

    Add files via upload

---
+++ PRAKTIKUM 4 CERPEN.py
@@ -0,0 +1,15 @@
+#BUNGA HARDIYANA PUTRI
+#2210312035
+#PRAKTIKUM 4 CERPEN
+
+kalimat_starter = "Hari minggu pagi ini, aku segera mandi dan bersiap untuk pergi."
+karakter = "Aku suka sekali membaca buku. Itu awalnya aku bisa berteman dengan Ryujin, karena kita memiliki hobi yang sama. Kita sering bertukar merekomendasikan buku."
+waktu = "Pukul 10.00 hari ini,"
+story_plot = "aku ada janji untuk bertemu sahabat kecilku setelah sekian lama kami berpisah."
+story_plot2 = "Setelah sampai di sana, aku langsung mengenali wajah Ryujin."
+tempat = "Aku bertemu dengannya di perpustakaan tempat kita membaca buku semasa kecil."
+second_character = "Tidak banyak berubah dari Ryujin, dia masih senang membaca buku dan menulis buku seperti dulu sama sepertiku."
+usia = "Saat ini, Ryujin berusia 17 tahun."
+pekerjaan = "Aku ingat, dahulu Ryujin pernah berkata akan menjadi penulis saat berumur 20 tahun nanti."
+
+print (kalimat_starter, waktu, story_plot, tempat, story_plot2, second_character, karakter, usia, pekerjaan)
